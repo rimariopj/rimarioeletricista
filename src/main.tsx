d990815abca696304f63cedde8d2a18d7c813d13
rimariopj <rimario.pj@gmail.com>
2026-04-26 16:15:00 -0300

    segundo up

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./app/App";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
-   <HashRouter>
-  <App />
-</HashRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </HelmetProvider>
 );
 
