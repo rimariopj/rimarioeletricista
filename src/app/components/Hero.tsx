d75f34a9e6e491f3ad14a62ab1e874ad49977d38
rimariopj <rimario.pj@gmail.com>
2026-04-26 15:31:19 -0300

    segundo up

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Phone, Clock, AlertTriangle } from "lucide-react";
-import logo from "./assets/Primus1.png";
-import heroBg from "./assets/hero-bg-vt.png";
+import logo from "assets/Primus1.png";
+import heroBg from "assets/hero-bg-vt.png";
 
 type HeroProps = {
   headline?: string;
